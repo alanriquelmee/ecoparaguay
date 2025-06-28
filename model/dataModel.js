8c23ec22c672fd15da8054ef683580c335910a17
Alan Riquelme <alanriack@gmail.com>
2025-06-28 12:08:04 -0300

    Agrego las carpetas de Modelo Vista Controlador al Proyecto Eco Paraguay

---
+++ model/dataModel.js
@@ -0,0 +1,9 @@
+export const users = [
+  { id: 1, name: "Juan Pérez", phone: "+595991112233" },
+  { id: 2, name: "Ana Gómez", phone: "+595992223344" }
+];
+
+export const messages = [
+  { id: 1, text: "Bienvenido a EcoParaguay!" },
+  { id: 2, text: "Su registro fue exitoso" }
+];
